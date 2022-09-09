465375f88c4986b42f2d82843b3b115bfb5ae997
fadayopaul <paulfadayo@yahoo.com>
2022-09-09 13:06:14 +0100

    add request loan integration

--- script.js
+++ script.js
@@ -203,7 +203,25 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
-// delete account
+// Loan money integration
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    currentAccount.movements.push(amount);
+
+    updateUI(currentAccount);
+  }
+
+  inputLoanAmount.value = "";
+});
+
+// delete account integrattion
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
 
