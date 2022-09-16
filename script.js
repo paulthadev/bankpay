d6e6356022e4150c5d9ee14a97aef0fc1e498736
fadayopaul <paulfadayo@yahoo.com>
2022-09-16 17:06:00 +0100

    Optimize date functionality

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    "2021-11-18T21:31:17.178Z",
-    "2021-12-23T07:42:02.383Z",
-    "2022-01-28T09:15:04.904Z",
-    "2022-04-01T10:17:24.185Z",
-    "2022-05-08T14:11:59.604Z",
-    "2022-05-27T17:01:17.194Z",
-    "2022-07-11T23:36:17.929Z",
-    "2022-07-12T10:51:36.790Z",
+    "2022-08-20T21:31:17.178Z",
+    "2022-08-23T07:42:02.383Z",
+    "2022-08-28T09:15:04.904Z",
+    "2022-09-11T10:17:24.185Z",
+    "2022-09-12T14:11:59.604Z",
+    "2022-09-13T17:01:17.194Z",
+    "2022-09-14T23:36:17.929Z",
+    "2022-09-16T10:51:36.790Z",
   ],
   currency: "EUR",
   locale: "pt-PT", // de-DE
@@ -32,14 +32,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    "2021-11-01T13:15:33.035Z",
-    "2021-11-30T09:48:16.867Z",
-    "2021-12-25T06:04:23.907Z",
-    "2022-01-25T14:18:46.235Z",
-    "2022-02-05T16:33:06.386Z",
-    "2022-04-10T14:43:26.374Z",
-    "2022-06-25T18:49:59.371Z",
-    "2022-07-26T12:01:20.894Z",
+    "2022-08-28T13:15:33.035Z",
+    "2022-08-30T09:48:16.867Z",
+    "2022-08-31T06:04:23.907Z",
+    "2022-09-11T14:18:46.235Z",
+    "2022-09-12T16:33:06.386Z",
+    "2022-09-14T14:43:26.374Z",
+    "2022-09-15T18:49:59.371Z",
+    "2022-09-16T12:01:20.894Z",
   ],
   currency: "USD",
   locale: "en-US",
@@ -76,6 +76,26 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////
 //FUNCTIONS
 
+// Creating the movement date function
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return "Today";
+  if (daysPassed === 1) return "Yesterday";
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    return `${day}-${month}-${year}`;
+  }
+};
+
 // creating the html templates & the transaction movements to the DOM
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = "";
@@ -88,11 +108,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-
-    const displayDate = `${day}-${month}-${year}`;
+    const displayDate = formatMovementDate(date);
 
     // Creating HTML template
     const html = `
